6fb2d2e2ce6875bed606b609eb10b354b97bef5b
Klaus Dollinger <klaus.dollinger@gmail.com>
2023-05-25 21:54:41 +0200

    Add socket.io and svelte client

---
+++ client/src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { io, Socket } from "socket.io-client";
+
+  const socket = io();
+
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
+
+  let txt: string;
+
+  function handle_submission() {
+    socket.emit("message", {
+      user_name: "Emil",
+      text: txt,
+      bot: false,
+    });
+  }
+</script>
+
+<h1>My First Test</h1>
+
+<form on:submit|preventDefault={handle_submission}>
+  <input type="text" bind:value={txt} />
+  <button>Send</button>
+</form>
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
+import {Server} from "socket.io"
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
@@ -8,6 +9,8 @@ const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+app.use(express.static("client/dist"))
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
@@ -18,6 +21,38 @@ app.get("/", async (req, res) => {
   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+//Socket.io
+type user_message = {
+  user_name: string;
+  text: string;
+  bot: false;
+}
+type bot_message = {
+  text: string;
+  bot: true;
+}
+type message = user_message | bot_message
+type client_to_server_events = {
+  message: (m: message) => void
+};
+type server_to_client_events = {
+  message: (m: message) => void
+};
+type inter_server_events = {};
+type socket_data = {};
+
+const io = new Server<client_to_server_events, server_to_client_events, inter_server_events, socket_data>(server);
+
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+
+  // receive a message
+  socket.on("message", (msg => {
+    io.emit("message", msg)
+  }))
 });
